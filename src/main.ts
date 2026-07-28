e88f5e05555a857698132e5dd487b5b9e5179a4a
Rafioul Hasan <rafioulhasan2@gmail.com>
2026-07-28 16:57:09 +0600

    fix: main.ts synced with config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { EnvConfig } from './config/env.schema';
 
 async function bootstrap() {
   console.log('Bootstrap starting...');
   try {
     const app = await NestFactory.create(AppModule);
-    console.log('Nest application created.');
-    await app.listen(process.env.PORT ?? 3000);
-    console.log(
-      'Nest application is listening on port:',
-      process.env.PORT ?? 3000,
-    );
+    const configService = app.get(ConfigService<EnvConfig, true>);
+    const port = configService.get('PORT', { infer: true });
+
+    await app.listen(port);
+    console.log(`🚀 Server running on http://localhost:${port}`);
   } catch (error) {
     console.error('Error during bootstrap:', error);
   }
