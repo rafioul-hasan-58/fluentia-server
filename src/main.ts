424ee4210e49b8ce6ef0144e8c8c1b0fb08d41fa
Rafioul Hasan <rafioulhasan2@gmail.com>
2026-07-28 16:43:33 +0600

    fix: prisma downgraded to 6

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  console.log('Bootstrap starting...');
+  try {
+    const app = await NestFactory.create(AppModule);
+    console.log('Nest application created.');
+    await app.listen(process.env.PORT ?? 3000);
+    console.log(
+      'Nest application is listening on port:',
+      process.env.PORT ?? 3000,
+    );
+  } catch (error) {
+    console.error('Error during bootstrap:', error);
+  }
 }
-bootstrap();
+void bootstrap();
