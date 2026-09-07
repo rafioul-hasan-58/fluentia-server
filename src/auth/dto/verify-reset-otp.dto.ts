6dc929a3474bc689cdbfc7b17336b8b0c567048a
Rafioul Hasan <rafioulhasan2@gmail.com>
2026-09-07 15:10:18 +0600

    otp redueced to 5 digit

--- src/auth/dto/verify-reset-otp.dto.ts
+++ src/auth/dto/verify-reset-otp.dto.ts
@@ -10,11 +10,11 @@ export class VerifyResetOtpDto {
   email!: string;
 
   @ApiProperty({
-    description: 'The 6-digit verification OTP code',
-    example: '123456',
+    description: 'The 5-digit verification OTP code',
+    example: '12345',
   })
   @IsString()
-  @Length(6, 6, { message: 'OTP must be exactly 6 digits' })
+  @Length(5, 5, { message: 'OTP must be exactly 5 digits' })
   @Matches(/^\d+$/, { message: 'OTP must contain only numbers' })
   otp!: string;
 }
